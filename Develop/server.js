fee9e413e25e7d079b7e8e154aba2373e56676eb
wdiste <Wdiste49@gmail.com>
2023-01-29 07:31:09 -0500

    changed path in index.html to URL

--- Develop/server.js
+++ Develop/server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 app.all('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 // route to use getNotes()
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/notes.json')));
