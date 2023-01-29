2966f786ca7c25c78f4154a615d4c9af438630f0
wdiste <Wdiste49@gmail.com>
2023-01-29 14:13:55 -0500

    removed app.all

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
-app.all('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+// app.all('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 
 // route to use getNotes()
