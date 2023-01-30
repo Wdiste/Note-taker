0d8ac67bd72c58ba878e98451122b8fadb157121
wdiste <Wdiste49@gmail.com>
2023-01-29 19:15:38 -0500

    added gets for css and javascript

--- Develop/server.js
+++ Develop/server.js
@@ -14,6 +14,9 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('/scripts', (req, res) => res.sendFile(path.join(__dirname, './public/assets/js/index.js')));
+app.get('/style', (req, res) => res.sendFile(path.join(__dirname, './public/assets/css/styles.css')));
+
 
 
 // route to use getNotes()
